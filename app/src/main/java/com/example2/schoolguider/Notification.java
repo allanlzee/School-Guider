ece4941f11f6b268a8b5b3d9e951e4104c6615ae
Allan Zhou <allanlinzhou@gmail.com>
2021-05-05 17:26:53 -0400

    Notification Activity Created

---
+++ app/src/main/java/com/example2/schoolguider/Notification.java
@@ -0,0 +1,92 @@
+package com.example2.schoolguider;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+
+import androidx.core.app.NotificationCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example2.schoolguider.databinding.NotificationActivityBinding;
+
+public class Notification extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private NotificationActivityBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Button notification;
+
+        binding = NotificationActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        /*
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_notification);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        notification = findViewById(R.id.notificationButton);
+
+        notification.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String message = "Notification Enabled for School Guider";
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                        Notification.this
+                )
+                        .setSmallIcon(R.drawable.ic_baseline_circle_notifications_24)
+                        .setContentTitle("Notification: School Guider")
+                        .setContentText(message)
+                        .setAutoCancel(true);
+
+                Intent intent = new Intent(Notification.this,
+                        SettingsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("message", message);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(Notification.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                builder.setContentIntent(pendingIntent);
+
+                NotificationManager notificationManager = (NotificationManager)getSystemService(
+                        Context.NOTIFICATION_SERVICE
+                );
+
+                notificationManager.notify(0, builder.build());
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_notification);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
--- app/src/main/java/com/example2/schoolguider/SettingsActivity.java
+++ app/src/main/java/com/example2/schoolguider/SettingsActivity.java
@@ -1,12 +1,22 @@
 package com.example2.schoolguider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
+import java.util.ArrayList;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    private Spinner notificationSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,41 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        notificationSpinner = (Spinner) findViewById(R.id.notificationSpinner);
+
+        ArrayList<String> activities = new ArrayList<String>();
+        activities.add("School Schedule");
+        activities.add("School Tracker");
+        activities.add("Mindfulness");
+
+
+        ArrayAdapter<String> notificationSpinnerAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, activities
+        );
+
+        notificationSpinner.setAdapter(notificationSpinnerAdapter);
+
+        // Set onClickListener
+        notificationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(SettingsActivity.this, activities.get(position) + " Selected", Toast.LENGTH_SHORT).show();
+                // TODO: mute notifications for the activity selected
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(SettingsActivity.this, "Select Muted Activities", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView textView = findViewById(R.id.notification);
+
+        String message = getIntent().getStringExtra("message");
+
+        textView.setText(message);
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -30,4 +75,5 @@ public class SettingsActivity extends AppCompatActivity {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
     }
+
 }
